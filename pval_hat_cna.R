d05e26ccb2e3522698f45f40fc67d206d545c52c
Veli-Pekka Parkkinen <parkkinenv@gmail.com>
2023-10-31 15:42:31 +0100

    typos...

--- pval_hat_cna.R
+++ pval_hat_cna.R
@@ -129,7 +129,7 @@ pval_hat_single <- function(model,
 jsre <- csf(cna(d.jobsecurity, con = .6, cov = .8, outcome = "JSR"))
   
 
-## try one of them for consistency pval
+## test one of them
 pval_hat_single("C + R <-> JSR", d.jobsecurity, obs_con = 0.722, obs_cov = 0.968,
                 nulltype = "perm.outcome")
 
