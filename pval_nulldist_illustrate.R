11da8389a983696536600e19c4e5a66a81bfbf53
Veli-Pekka Parkkinen <parkkinenv@gmail.com>
2023-11-20 09:54:08 +0100

    each model has their own nonsense dataset

--- pval_nulldist_illustrate.R
+++ pval_nulldist_illustrate.R
@@ -11,7 +11,7 @@ if(!require(rstudioapi)){
   setwd(dirname(path))
 }
 
-
+#library(cnaOpt)
 library(cnasimtools)
 library(doParallel)
 source("pval_hat_cna.R")
@@ -21,21 +21,39 @@ dcluster <- makeCluster(n.cores, type = "FORK")
 registerDoParallel(cl = dcluster)
 
 
-ndat <- bs_dat_create(Nsets = 1, type = "cs")[[1]]
+N <- 1000 # increase this to get a better estimate of the p-val distr.
+vn <- 7
+#ndat <- bs_dat_create(Nsets = 1, type = "cs")[[1]]
+ndat <- bs_dat_create(Nsets = N, varnum = vn ,type = "cs")
 # WARNING: will take time to run
-N <- 100 # increase this to get a better estimate of the p-val distr.
-models <- replicate(N, randomCsf(ndat))
+#models <- replicate(N, randomCsf(ndat))
+models <- replicate(N, randomCsf(vn))
 
-fits <- mclapply(models, function(x) condition(x, ndat))
+#fits <- mclapply(models, function(x) condition(x, ndat))
+fits <- mcmapply(condition, models, ndat, SIMPLIFY = FALSE)
+
 fits <- mclapply(fits, function(x) attributes(x)$info[,c("consistency", "coverage")]) 
+fits <- lapply(fits, 
+              function(f) {f[is.na(f)] <- 0L ; return(f)})
+
+
+# pvals <- foreach(i = 1:N) %dopar% {
+#   pval_hat_single(models[[i]],
+#                   ndat,
+#                   obs_con = fits[[i]]$consistency,
+#                   obs_cov = fits[[i]]$coverage,
+#                   nulltype = "iid",
+#                   bs_samples = 100)
+# }
+
 
 pvals <- foreach(i = 1:N) %dopar% {
   pval_hat_single(models[[i]],
-                  ndat,
+                  ndat[[i]],
                   obs_con = fits[[i]]$consistency,
                   obs_cov = fits[[i]]$coverage,
                   nulltype = "iid",
-                  bs_samples = 100)
+                  bs_samples = 10000)
 }
 
 
@@ -49,16 +67,16 @@ pv_cov_no <- na.omit(pvals_cov)
 
 # any(duplicated(pv_con_no))
 # which(duplicated(pv_cov_no))
-# pv_con_no <- pv_con_no + runif(length(pv_con_no), min = -0.001, max = 0.001)
+ pv_con_no <- pv_con_no + runif(length(pv_con_no), min = -0.001, max = 0.001)
 #  
-# pv_cov_no <- pv_cov_no + runif(length(pv_cov_no), min = -0.001, max = 0.001)
+ pv_cov_no <- pv_cov_no + runif(length(pv_cov_no), min = -0.001, max = 0.001)
 
 # kolmogorov-smirnov, is the distr of p-values different from uniform
 ks.test(pv_con_no, "punif")
 ks.test(pv_cov_no, "punif")
 
 # chi-squared. Why are the results so different? Which one, if either, is appropriate?
-chisq.test(pv_con_no, p = rep(1/length(pv_con_no), length(pv_con_no)))
+chisq.test(pv_con_no)
 chisq.test(pv_cov_no, p = rep(1/length(pv_cov_no), length(pv_cov_no)))
 
 # length(which(pvals_con >= 0.99))
